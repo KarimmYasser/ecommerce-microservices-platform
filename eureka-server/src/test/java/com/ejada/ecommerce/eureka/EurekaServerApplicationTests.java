11c7d48837631def7e99e14239267bbd78db8346
Karim Yasser <karimmyasserr@gmail.com>
2026-07-24 04:56:19 +0300

    feat(eureka-server): add service discovery server
    Self-preservation disabled for local development so dead instances drop
    quickly. All other modules register here and resolve each other by name.

---
+++ eureka-server/src/test/java/com/ejada/ecommerce/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejada.ecommerce.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
